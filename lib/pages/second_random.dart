98c100fc64de071e4ca5838375acb1f08d549f21
s4llm4n <s4llm4n@gmail.com>
2023-08-02 15:14:11 +0700

    finish

--- lib/pages/second_random.dart
+++ lib/pages/second_random.dart
@@ -28,37 +28,123 @@ class SecondRandom extends StatelessWidget {
             ),
           ),
           SizedBox(height: 37,),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'About',
-                style: GoogleFonts.poppins(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-            ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'About',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
               ),
-              Text(
-                'Pantai Pandawa adalah salah satu para \nkawasan wisata di area Kuta selatan sana \nKabupaten Dekat Bandung, Bali',
-                style: GoogleFonts.poppins(
-                  color: Color(0XFF2F323A),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w300,
-            ),
+                ),
+                Text(
+                  'Pantai Pandawa adalah salah satu para \nkawasan wisata di area Kuta selatan sana \nKabupaten Dekat Bandung, Bali',
+                  style: GoogleFonts.poppins(
+                    color: Color(0XFF2F323A),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
               ),
-              SizedBox(height: 26,),
-              Text(
-                'Booking Now',
-                style: GoogleFonts.poppins(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-            ),
+                ),
+                SizedBox(height: 26,),
+                Text(
+                  'Booking Now',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
               ),
-            ],
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/date_one.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(width: 20,),
+                    Image.asset(
+                      'assets/date_two.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(width: 20,),
+                    Image.asset(
+                      'assets/date_three.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(width: 20,),
+                    Image.asset(
+                      'assets/date_four.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20),
+        child: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Column(
+                children: [
+                  Text(
+                  '\$22,800',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xff3F6DF6),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ),
+                  Text(
+                    '/night',
+                    style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  ),
+                ],
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 220,
+                height: 60,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff3F6DF6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(19),
+                    ),
+                  ),
+                  onPressed: () {}, 
+                  child: Text(
+                    'Continue',
+                    style: GoogleFonts.poppins(
+                      color: Color(0xFAFAFAff),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              label: '',
+            ),
+        ],
+        ),
       ),
     );
   }
